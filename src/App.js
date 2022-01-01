ed6386c62062cdfdaddde3fbb5e59cc3512a9fe7
shawnelldh <shawnelldh@gmail.com>
2022-01-01 12:18:05 -0500

    3 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./style.css";
+import Header from './components/Header';
+import Body from './components/Body';
 
 function App() {
  return (
    <div className="App">
      <h1>My App</h1>
+     <Header/>
+     <Body/>
      </div>
  );
 
