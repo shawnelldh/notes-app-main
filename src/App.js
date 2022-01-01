b8aa331f82e744ebdd476e535a46c8d10d627ae6
shawnelldh <shawnelldh@gmail.com>
2022-01-01 11:46:28 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+function App() {
+ return (
+   <div className="App">
+     <h1>My App</h1>
+     </div>
+ );
+
 }
+
+export default  App;
